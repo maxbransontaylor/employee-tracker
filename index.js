b82b264e77ca092753bd44960c7e9fef0f89991e
Max Taylor <maxbransontaylor@gmail.com>
2022-08-19 20:47:16 -0400

    took about 300 years to figure out how to nest db and inquirer statements in order to create add new employee functionality"

--- index.js
+++ index.js
@@ -52,10 +52,78 @@ function deleteEmployee() {
 
 // Query database
 function showEmployees() {
-  db.query("SELECT * FROM employee", function (err, results) {
-    console.table(results);
-    showOptions();
+  db.query(
+    `SELECT employee.id,employee.first_name,employee.last_name,CONCAT(manager.first_name," ", manager.last_name) AS managerName,role.title,role.salary,department.name AS department  
+  FROM employee employee LEFT JOIN employee manager ON employee.manager_id=manager.id
+ LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id;`,
+    function (err, results) {
+      console.table(results);
+      showOptions();
+    }
+  );
+}
+
+function addEmployee() {
+  let managerChoices = [];
+  let roleChoices = [];
+  db.query(`SELECT * FROM role`, function (err, results) {
+    roleChoices = results.map(({ id, title }) => {
+      return {
+        value: id,
+        name: title,
+      };
+    });
+    db.query(`SELECT * FROM employee`, (err, results) => {
+      managerChoices = results.map(({ id, first_name, last_name }) => {
+        return { name: `${first_name} ${last_name}`, value: id };
+      });
+      managerChoices.push({ name: "none", value: null });
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "role",
+            message: "What is the employee's role?",
+            choices: roleChoices,
+          },
+          {
+            type: "input",
+            name: "first_name",
+            message: "Enter employee's first name:",
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "Enter employee's last name:",
+          },
+          {
+            type: "list",
+            name: "manager",
+            message: "Who is the employee's manager?",
+            choices: managerChoices,
+          },
+        ])
+        .then((answers) => {
+          const sql = `INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)`;
+          const params = [
+            answers.first_name,
+            answers.last_name,
+            answers.role,
+            answers.manager,
+          ];
+          db.query(sql, params, (err, result) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log(result);
+            showOptions();
+          });
+        });
+    });
   });
+  console.log(roleChoices);
 }
 
 function showOptions() {
@@ -65,7 +133,12 @@ function showOptions() {
         type: "list",
         name: "choice",
         message: "What would you like to do?",
-        choices: ["View All Employees", "Remove Employee", "Quit"],
+        choices: [
+          "View All Employees",
+          "Add an Employee",
+          "Remove Employee",
+          "Quit",
+        ],
       },
     ])
     .then(({ choice }) => {
@@ -75,6 +148,9 @@ function showOptions() {
           break;
         case "Remove Employee":
           deleteEmployee();
+          break;
+        case "Add an Employee":
+          addEmployee();
           break;
         default:
           process.exit();
